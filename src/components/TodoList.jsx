0120718ac5c20429c4f86431c212088743fe86ae
minseong-dev <aa971020@gmail.com>
2025-06-12 16:11:04 +0900

    ADD : Todo 검색기능

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,14 +1,20 @@
 import styled from "styled-components";
 import TodoItem from "./TodoItem";
-import { useRecoilValue } from "recoil";
-import { sortedTodoList } from "../recoil/todo/sortSelector";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { searchedTodoList } from "../recoil/todo/searchSelector";
+import { searchState } from "../recoil/todo/searchState";
 
 const TodoList = () => {
-    const todoList = useRecoilValue(sortedTodoList);
+    const todoList = useRecoilValue(searchedTodoList);
+    const setSearch = useSetRecoilState(searchState);
+
+    const onChangeSeach = (e) => {
+        setSearch(e.target.value);
+    };
 
     return (
         <Wrapper>
-            <Input placeholder="검색어를 입력하세요" />
+            <Input placeholder="검색어를 입력하세요" onChange={onChangeSeach} />
             {todoList.map((todo) => (
                 <TodoItem key={todo.id} item={todo} />
             ))}
---
+++ src/recoil/todo/searchSelector.js
@@ -0,0 +1,17 @@
+import { selector } from "recoil";
+import { sortedTodoList } from "./sortSelector";
+import { searchState } from "./searchState";
+
+export const searchedTodoList = selector({
+    key: "searchedTodoList",
+    get: ({ get }) => {
+        const todoList = get(sortedTodoList);
+        const keyword = get(searchState).toLowerCase().trim();
+
+        if (!keyword) return todoList;
+
+        return todoList.filter((todo) =>
+            todo.content.toLowerCase().includes(keyword)
+        );
+    },
+});
